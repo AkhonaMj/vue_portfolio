95f808b6c20b82b218a5f1f7649ee4d549cf3b71
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-07-03 16:04:28 +0200

    added resume

--- src/components/ResumeComp.vue
+++ src/components/ResumeComp.vue
@@ -1,16 +1,117 @@
-<template lang="">
-<div class="image-background" :style="{ backgroundImage: `url(${imageUrl})` }">
-
-    <div>
-        
+<template>
+    <div class="image-background" :style="{ backgroundImage: `url(${imageUrl})` }">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="card">
+              <div class="card-body">
+                <h2>Education</h2>
+                <div class="hori-timeline" dir="ltr">
+                  <ul class="list-inline events">
+                    <li class="list-inline-item event-list" v-for="education in education" :key="education.year">
+                      <div class="px-4">
+                        <h4>{{ education.year }}</h4>
+                        <h5>{{ education.institution }}</h5>
+                        <h6>{{ education.description }}</h6>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="card">
+              <div class="card-body">
+                <h2>Work Eperience</h2>
+                <div class="hori-timeline" dir="ltr">
+                  <ul class="list-inline events">
+                    <li class="list-inline-item event-list" v-for="workExp in workExp" :key="workExp.year">
+                      <div class="px-4">
+                        <h4>{{ workExp.year }}</h4>
+                        <h5>{{ workExp.placeOfWork }}</h5>
+                        <h6>{{ workExp.description }}</h6>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-</template>
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        imageUrl: "https://akhonamj.github.io/portfolio_images/plain-beige-myryswi114smviw6.jpg"
+      }
+    },
+    computed: {
+      education() {
+        return this.$store.state.education;
+      },
+      workExp(){
+        return this.$store.state.workExp;
+      }
+    },
+    methods: {
+      getApiData() {
+        this.$store.dispatch('getApiData');
+      }
+    },
+    mounted() {
+      this.getApiData();
+    }
+  }
+  </script>
+  
+  <style>
+  body {
+    background: #eee;
+    margin-top: 20px;
+  }
+  
+  .hori-timeline .events {
+    border-top: 3px solid deeppink;
+  }
+  
+  .hori-timeline .events .event-list {
+    display: block;
+    position: relative;
+    text-align: center;
+  }
+  
+  .hori-timeline .events .event-list:before {
+    content: "";
+    position: absolute;
+    height: 36px;
+    border-right: 2px dashed deeppink;
+    top: 0;
+  }
+  
+  @media (min-width: 1140px) {
+    .hori-timeline .events .event-list {
+      display: inline-block;
+      width: 24%;
+      padding-top: 45px;
+    }
+    .card {
+      border: none;
+      margin-bottom: 24px;
+      width: 100%;
+      max-width: 100%;
+      font-size: medium;
+    }
     
-}
-</script>
-<style lang="">
-    
-</style>
+  }
+  
+  </style>
+  
